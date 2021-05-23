0ae718a05ba4327515da740b5d308004fa97ff14
Stephanie Hyland <steph.hyland@gmail.com>
2021-05-23 20:08:33 +0100

    colors too

--- make_links.sh
+++ make_links.sh
@@ -5,3 +5,5 @@ mv -v ~/.vimrc ~/.vimrc.old
 
 ln -sv $dotfiledir/.tmux.conf ~
 ln -sv $dotfiledir/.vimrc ~
+mkdir -p $dotfiledir/.vim/colors
+ln -sv $dotfiledir/badwolf.vim ~/.vim/colors/badwolf.vim
