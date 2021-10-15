de13c36535ebf345d3865e11f8bc53db16a03227
Stephanie Hyland <stephanie.hyland@microsoft.com>
2021-10-15 14:42:16 +0000

    yep

--- make_links.sh
+++ make_links.sh
@@ -2,8 +2,10 @@ dotfiledir=`pwd`
 
 mv -v ~/.tmux.conf ~/.tmux.conf.old
 mv -v ~/.vimrc ~/.vimrc.old
+mv -v ~/.vim ~/.vim-old
 
 ln -sv $dotfiledir/.tmux.conf ~
 ln -sv $dotfiledir/.vimrc ~
-mkdir -p $dotfiledir/.vim/colors
-ln -sv $dotfiledir/badwolf.vim ~/.vim/colors/badwolf.vim
+#mkdir -p $dotfiledir/.vim/colors
+ln -s $dotfiledir/.vim  ~
+#ln -sv $dotfiledir/badwolf.vim ~/.vim/colors/badwolf.vim
